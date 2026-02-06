25b25570018badd19f875dd8825ac704842c2105
Thomas <github@als.bochmail.de>
2026-02-06 01:40:13 +0100

    style(ButtonGroup): update theme styles to use !important for rounded classes bcs of ct conflicts

--- packages/prime-volt/src/volt/ButtonGroup.vue
+++ packages/prime-volt/src/volt/ButtonGroup.vue
@@ -24,7 +24,7 @@ interface Props extends /* @vue-ignore */ ButtonGroupProps {}
 defineProps<Props>();
 
 const theme = ref<ButtonGroupPassThroughOptions>({
-  root: `*:rounded-none *:first:rounded-s-md *:last:rounded-e-md
+  root: `*:!rounded-none *:first:!rounded-s-md *:last:!rounded-e-md
         *:focus-visible:relative *:focus-visible:z-10 *:not-last:border-r-0`,
 });
 </script>
